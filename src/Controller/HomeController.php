60783a770bf0b3a03c46fa830b6500078d17b52c
D4GNIR <jeremiecette@gmail.com>
2022-05-26 08:40:03 +0200

    ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private PokemonRepository $pokemonRepository;
+    private PaginatorInterface $paginator;
+    private EntityManagerInterface $em;
+
+
+    public function __construct(
+        PokemonRepository $pokemonRepository,
+        PaginatorInterface $paginator,
+        EntityManagerInterface $em,
+    ) {
+        $this->pokemonRepository = $pokemonRepository;
+        $this->paginator = $paginator;
+        $this->em = $em;
+    }
+
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $qb = $this->pokemonRepository->getQbAll();
+
+        $pagination = $this->paginator->paginate(
+            $qb, //La query
+            $request->query->getInt('page',1), //Le numero de page de depart
+            9 //Le nombre de résultat pas page
+        );
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'pagination' => $pagination,
         ]);
     }
 }
